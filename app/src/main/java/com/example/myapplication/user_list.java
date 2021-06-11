7f2c64e21f3584af4b1640ed83cfb282b191b353
Rimjhim1710 <85617076+Rimjhim1710@users.noreply.github.com>
2021-06-11 23:39:14 +0530

    Update user_list.java

--- app/src/main/java/com/example/myapplication/user_list.java
+++ app/src/main/java/com/example/myapplication/user_list.java
@@ -6,7 +6,7 @@ import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MenuItem; 
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
