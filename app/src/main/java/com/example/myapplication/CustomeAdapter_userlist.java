5c67488ff2fb6f7361f94598f50785dc86d7d92d
Rimjhim1710 <85617076+Rimjhim1710@users.noreply.github.com>
2021-06-11 23:29:37 +0530

    Update CustomeAdapter_userlist.java

--- app/src/main/java/com/example/myapplication/CustomeAdapter_userlist.java
+++ app/src/main/java/com/example/myapplication/CustomeAdapter_userlist.java
@@ -5,7 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView; 
 
 import java.util.List;
 
