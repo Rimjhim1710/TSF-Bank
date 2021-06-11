0b2802ec33b17077b8aec6684f354de648a12337
Rimjhim1710 <85617076+Rimjhim1710@users.noreply.github.com>
2021-06-11 23:33:20 +0530

    Update ViewHolder.java

--- app/src/main/java/com/example/myapplication/ViewHolder.java
+++ app/src/main/java/com/example/myapplication/ViewHolder.java
@@ -5,7 +5,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView; 
 
 public class ViewHolder extends RecyclerView.ViewHolder {
 
