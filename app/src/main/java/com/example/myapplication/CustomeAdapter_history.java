f39cec6e3139a36a73e0c5fc395d00e7853f72b1
Rimjhim1710 <85617076+Rimjhim1710@users.noreply.github.com>
2021-06-11 23:26:03 +0530

    Update CustomeAdapter_history.java

--- app/src/main/java/com/example/myapplication/CustomeAdapter_history.java
+++ app/src/main/java/com/example/myapplication/CustomeAdapter_history.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 import android.content.Context;
 import android.graphics.Color;
 import android.view.LayoutInflater;
-import android.view.View;
+import android.view.View; 
 import android.view.ViewGroup;
 import android.widget.TextView;
 
