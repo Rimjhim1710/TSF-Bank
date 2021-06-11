b5123cff197c5c14734fdee9a34b08907a1b0446
Rimjhim1710 <85617076+Rimjhim1710@users.noreply.github.com>
2021-06-11 23:37:15 +0530

    Update splash_screen.java

--- app/src/main/java/com/example/myapplication/splash_screen.java
+++ app/src/main/java/com/example/myapplication/splash_screen.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.view.animation.AnimationUtils;
+import android.view.animation.AnimationUtils; 
 import android.widget.ImageView;
 import android.widget.TextView;
 
