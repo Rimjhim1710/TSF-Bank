a5457d9873bc2314a7d5a5e222fd7a14f76b37b1
Rimjhim1710 <85617076+Rimjhim1710@users.noreply.github.com>
2021-06-11 23:38:57 +0530

    Update user_data.java

--- app/src/main/java/com/example/myapplication/user_data.java
+++ app/src/main/java/com/example/myapplication/user_data.java
@@ -4,7 +4,7 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
-import android.os.Bundle;
+import android.os.Bundle; 
 import android.util.Log;
 import android.view.View;
 import android.widget.Button; 
