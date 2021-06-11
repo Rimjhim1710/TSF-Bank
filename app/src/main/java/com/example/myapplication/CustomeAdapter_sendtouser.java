ca3b6fd496389e587d8e7c6aa1ed15c814a9c8ff
Rimjhim1710 <85617076+Rimjhim1710@users.noreply.github.com>
2021-06-11 23:29:20 +0530

    Update CustomeAdapter_sendtouser.java

--- app/src/main/java/com/example/myapplication/CustomeAdapter_sendtouser.java
+++ app/src/main/java/com/example/myapplication/CustomeAdapter_sendtouser.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull; 
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView; 
 
 import java.util.ArrayList;
 import java.util.List;
