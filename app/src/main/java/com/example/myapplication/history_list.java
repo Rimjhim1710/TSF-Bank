2786e511543fa9d610ee902060c95309fa71090d
Rimjhim1710 <85617076+Rimjhim1710@users.noreply.github.com>
2021-06-11 23:33:39 +0530

    Update history_list.java

--- app/src/main/java/com/example/myapplication/history_list.java
+++ app/src/main/java/com/example/myapplication/history_list.java
@@ -5,7 +5,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.view.View; 
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
